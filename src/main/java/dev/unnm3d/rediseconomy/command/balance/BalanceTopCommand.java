0265fbd4a3429f3710c32eeac478a94f40c0f9fa
Emibergo02 <36164338+Emibergo02@users.noreply.github.com>
2023-10-30 19:08:23 +0100

    4.3.7c

--- src/main/java/dev/unnm3d/rediseconomy/command/balance/BalanceTopCommand.java
+++ src/main/java/dev/unnm3d/rediseconomy/command/balance/BalanceTopCommand.java
@@ -24,13 +24,14 @@ public class BalanceTopCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-        final int page;
-        try {
-            page = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            plugin.langs().send(sender, plugin.langs().missingArguments);
-            return true;
-        }
+        int page = 1;
+        if (args.length == 1)
+            try {
+                page = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                plugin.langs().send(sender, plugin.langs().missingArguments);
+                return true;
+            }
 
         Currency baltopCurrency = args.length == 2 ? currenciesManager.getCurrencyByName(args[1]) : currenciesManager.getDefaultCurrency();
 
@@ -38,33 +39,36 @@ public class BalanceTopCommand implements CommandExecutor, TabCompleter {
             plugin.langs().send(sender, plugin.langs().invalidCurrency);
             return true;
         }
+
+        final int finalPage = page;
         //Baltop paging, 10 per page
-        baltopCurrency.getOrderedAccounts(200).thenApply(balances -> {
+        baltopCurrency.getOrderedAccounts(200)
+                .thenApply(balances -> {
+                    List<ScoredValue<String>> pageBalances;
+                    if (balances.size() < (finalPage - 1) * 10) {//If the page is higher that the balances available
+                        return new ArrayList<ScoredValue<String>>();
+                    } else if (balances.size() > finalPage * 10) {
+                        pageBalances = balances.subList((finalPage - 1) * 10, finalPage * 10);
+                    } else pageBalances = balances.subList((finalPage - 1) * 10, balances.size());
+                    //Page formatting: clickable arrows to go to next/previous page
+                    plugin.langs().send(sender, plugin.langs().balanceTop.replace("%page%", String.valueOf(finalPage))
+                            .replace("%nextpage%", "<click:run_command:/balancetop " + (balances.size() <= finalPage * 10 ? 1 : finalPage + 1) + ">-></click>")
+                            .replace("%prevpage%", "<click:run_command:/balancetop " + (finalPage == 1 ? (balances.size() % 10) + 1 : finalPage - 1) + "><-</click>"));
 
-            List<ScoredValue<String>> pageBalances;
-            if (balances.size() < (page - 1) * 10) {//If the page is higher that the balances available
-                return new ArrayList<ScoredValue<String>>();
-            } else if (balances.size() > page * 10) {
-                pageBalances = balances.subList((page - 1) * 10, page * 10);
-            } else pageBalances = balances.subList((page - 1) * 10, balances.size());
-            //Page formatting: clickable arrows to go to next/previous page
-            plugin.langs().send(sender, plugin.langs().balanceTop.replace("%page%", String.valueOf(page))
-                    .replace("%nextpage%", "<click:run_command:/balancetop " + (balances.size() <= page * 10 ? 1 : page + 1) + ">-></click>")
-                    .replace("%prevpage%", "<click:run_command:/balancetop " + (page == 1 ? (balances.size() % 10) + 1 : page - 1) + "><-</click>"));
-
-            return new PageData(page, pageBalances);
-        }).thenAccept((data) -> {
-            PageData pageData = (PageData) data;
-            int i = 1;
-            for (ScoredValue<String> tuple : pageData.pageBalances) {
-                String username = currenciesManager.getUsernameFromUUIDCache(UUID.fromString(tuple.getValue()));
-                plugin.langs().send(sender, plugin.langs().balanceTopFormat
-                        .replace("%pos%", String.valueOf((pageData.pageNumber - 1) * 10 + i))
-                        .replace("%player%", username == null ? tuple.getValue() + "-Unknown" : username)
-                        .replace("%balance%", baltopCurrency.format(tuple.getScore())));
-                i++;
-            }
-        });
+                    return new PageData(finalPage, pageBalances);
+                })
+                .thenAccept((data) -> {
+                    PageData pageData = (PageData) data;
+                    int i = 1;
+                    for (ScoredValue<String> tuple : pageData.pageBalances) {
+                        String username = currenciesManager.getUsernameFromUUIDCache(UUID.fromString(tuple.getValue()));
+                        plugin.langs().send(sender, plugin.langs().balanceTopFormat
+                                .replace("%pos%", String.valueOf((pageData.pageNumber - 1) * 10 + i))
+                                .replace("%player%", username == null ? tuple.getValue() + "-Unknown" : username)
+                                .replace("%balance%", baltopCurrency.format(tuple.getScore())));
+                        i++;
+                    }
+                });
         return true;
     }
 
